63fc2b3e4ef156c28d40465a66c143131aec6b42
Saaanzio <rafaelsanziosilva22@gmail.com>
2024-05-09 22:14:28 -0300

    removed unused imports

--- src/Visao/Tela.java
+++ src/Visao/Tela.java
@@ -2,8 +2,6 @@ package Visao;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Tela extends JFrame {
     public Tela(){
